ad496636ec369c8f7de35e40647d9d4bdb4fb807
narsyn <94863191+narsyn@users.noreply.github.com>
2022-04-20 08:59:57 +0200

    Update DomainDecomposition.py

--- lib/DomainDecomposition.py
+++ lib/DomainDecomposition.py
@@ -1085,6 +1085,7 @@ def bounding_Box_2D(data,index,matrix_size):
     upper = index[0] % matrix_size
 
     for x in index:
+        print(x)
         x_mod = x % matrix_size
     if x_mod < lower:
         lower = x_mod
