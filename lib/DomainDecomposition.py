cffb3cf4fc8236d7510b646b16779264bdbe6b5a
narsyn <94863191+narsyn@users.noreply.github.com>
2022-04-26 10:31:45 +0200

    Update DomainDecomposition.py

--- lib/DomainDecomposition.py
+++ lib/DomainDecomposition.py
@@ -449,7 +449,7 @@ def DomDecIteration_KeOps(\
         arrayAdder.add(x,y)
     muYCellData,muYCellIndices=arrayAdder.getDataTuple()
     
-    print(length(muYCellIndices))
+    print(len(muYCellIndices))
 
     # another dummy return and dummy function call
     #SolveOnCell(muXCell,muYCellData,muYCellIndices,posXCell,posY,muXCell,muY,alphaCell,eps)
@@ -458,7 +458,7 @@ def DomDecIteration_KeOps(\
     #convert to bounding Box 
     muYCellDataBox,muYCellIndicesBox = bounding_Box_2D(muYCellData,muYCellIndices,512)
     
-    print(length(muYCellIndicesBox))
+    print(len(muYCellIndicesBox))
     
     print("passed bounding Box")
 
