80f4dd473ae6b98d94241b05e252e126f74ad9d8
narsyn <94863191+narsyn@users.noreply.github.com>
2022-06-07 13:49:15 +0200

    test print

--- lib/DomainDecomposition.py
+++ lib/DomainDecomposition.py
@@ -439,7 +439,9 @@ def SolveOnCellKeopsGrid(muX,muY,subMuY,subY,posX,posY,rhoX,rhoY,alphaInit,eps,S
 
     #TODO generalize this
     dx =  (len(posX)**1/dim)/2
-        
+    print("posX")
+    print(posX)
+    print(len(posX))
     # ----------------
     # With new softmin-grid
     # For images, it is assumed that 0th dimension is batch dimension, 1st is channel, 
