34e8d9a2ae7a10ad37160cdbfa1f61c9fc1afe49
narsyn <94863191+narsyn@users.noreply.github.com>
2022-06-21 13:52:26 +0200

    typo fix

--- lib/DomainDecomposition.py
+++ lib/DomainDecomposition.py
@@ -197,7 +197,7 @@ def BatchIterate(\
     muXListBatch = np.reshape(muXList,(nBatch,BatchSize,len(muXList[0]))).tolist()
     posXListBatch = np.reshape(posXList,(nBatch,BatchSize,len(muXList[0]),dim)).tolist()
     alphaListBatch = np.reshape(alphaList,(nBatch,BatchSize,len(muXList[0]))).tolist()
-    partitionDataCompCellIndicesBatch = np.reshape(partitionDataCompCellIndices,(nBatch,BatchSize,cellSize,dim))).tolist()
+    partitionDataCompCellIndicesBatch = np.reshape(partitionDataCompCellIndices,(nBatch,BatchSize,cellSize,dim)).tolist()
     partitionDataCompCellsBatch = np.reshape(partitionDataCompCells,(nBatch,BatchSize,cellSize)).tolist()
 
     for i in range(nBatch):
