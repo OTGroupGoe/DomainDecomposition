7ae4dc152219ee699ef530c29a7a849f67ababc7
narsyn <94863191+narsyn@users.noreply.github.com>
2022-06-27 14:48:25 +0200

    Update DomainDecomposition.py

--- lib/DomainDecomposition.py
+++ lib/DomainDecomposition.py
@@ -619,7 +619,7 @@ def BatchSolveOnCell_KeopsGrid(muX,subMuY,subY,posX,posY,rhoX,rhoY,alphaInit,eps
     print(alpha[i][0].size())
     
     offset_alpha = offset_alpha.view(BatchSize,1,(2*cellsize)**2)
-    offset_beta = offset_beta.view(BatchSize,1,(2*cellsize)**2)
+    offset_beta = offset_beta.view(BatchSize,1,boxDim[0]*boxDim[1])
     
     alpha = 2*alpha + 2*offset_alpha
     beta = 2*beta + 2*offset_beta + torch.sum((offset_x - offset_y)**2)
