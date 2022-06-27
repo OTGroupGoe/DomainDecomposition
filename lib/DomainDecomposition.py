79d58b64bec2df7ea333b41fb81b9384948acc8e
narsyn <94863191+narsyn@users.noreply.github.com>
2022-06-27 09:53:32 +0200

    Update DomainDecomposition.py

--- lib/DomainDecomposition.py
+++ lib/DomainDecomposition.py
@@ -497,8 +497,8 @@ def BatchSolveOnCell_KeopsGrid(muX,subMuY,subY,posX,posY,rhoX,rhoY,alphaInit,eps
     dim = posX[0].shape[1]
     cellsize = int(posX[0].shape[0]**(1/dim) / 2)
     
-    
-    print(posX.shape())
+    print(len(posX))
+    print(posX[].shape())
     print("------------------------")
     
     offset_x = torch.tensor([posX[i][0,:] for i in range(BatchSize)]).cuda()
