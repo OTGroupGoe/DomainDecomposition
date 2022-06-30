cb37f59f3e4f4cde89462b654925e1e2cdbbf79f
Ismael Medina <ismaelmedinasuarez@gmail.com>
2022-06-30 15:04:57 +0200

    debugging

--- lib/DomainDecomposition.py
+++ lib/DomainDecomposition.py
@@ -640,7 +640,10 @@ def BatchSolveOnCell_KeopsGrid(muX,subMuY,subY,posX,posY,rhoX,rhoY,alphaInit,eps
     # \nu_j^c = \sum_i \mu_i^c * exp((\alpha_i^c + \beta_j - cost(x_i^c, y_j))/eps) \nu_j
     log_rho = (L_logmuX + L_alpha/eps - C_ij/eps).logsumexp(2) # has shape (4, NY), 4 for the number of cells
     log_rho = log_rho.view(BatchSize, 4, -1)
-    print(KeposX)
+    print("KeposX", KeposX)
+    print("logmuX", log_dens(KemuX))
+    print("eps", eps)
+    print("Y", KesubPosY)
     print("L_logmuX: ", torch.sum(log_dens(KemuX).view(4, -1), axis = 1))
     print("log_rho: ", torch.sum(log_rho, axis = -1))
     P = torch.exp(beta.view(BatchSize, 1, -1)/eps + log_rho + log_dens(KesubRhoY.view(BatchSize, 1, -1)))
