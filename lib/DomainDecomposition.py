c3e8780f65ee56097255443882e3eca237dbcb61
narsyn <94863191+narsyn@users.noreply.github.com>
2022-07-04 10:11:39 +0200

    Update DomainDecomposition.py

--- lib/DomainDecomposition.py
+++ lib/DomainDecomposition.py
@@ -407,7 +407,7 @@ def BatchSolveOnCell_KeopsGrid(muX,subMuY,subY,posX,posY,rhoX,rhoY,alphaInit,eps
     cellsize = int(posX[0].shape[0]**(1/dim) / 2)
     
     # TODO offset is ineffectivly calculated, maybe use smt like posX[:,0,:]
-    print(posX.shape)
+    print(posX.shape())
     offset_x = torch.tensor(posX[:,0,:]).reshape(BatchSize,1,dim)
     KeposX = torch.tensor(posX).cuda() - offset_x
     KemuX = torch.tensor(muX).cuda()
